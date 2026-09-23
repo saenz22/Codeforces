f5856f3fb40c7d7218df0f5ae8d64d04c7913b6e
SebastianSaenz <saenz.sebastian@correounivalle.edu.co>
2026-09-23 17:42:09 -0500

    Bit progress in rmq, and started a new problem, aos. The main goal today is to finish rmq and start to get into aos

---
+++ Exercises/AndOrSort/aos.cpp
@@ -0,0 +1,22 @@
+// And, Or and Sort! in C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //int t;
+    //cin >> t;
+
+    //while (t--) {
+        solve();
+    //}
+
+    return 0;
+}
